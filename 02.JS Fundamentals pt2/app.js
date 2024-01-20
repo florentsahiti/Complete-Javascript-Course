c437fe4cacfb62c76de036dd57e4e8fbf4ad37ad
florentsahiti <66284617+florentsahiti@users.noreply.github.com>
2024-01-20 17:31:19 +0100

    loops

--- 02.JS Fundamentals pt2/app.js
+++ 02.JS Fundamentals pt2/app.js
@@ -306,4 +306,100 @@
 
 
 
-////// Looping Arrays, Breaking and Continuing ///////
+/////// Looping Arrays, Breaking and Continuing /////////
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//      2037 - 1991,
+//      'teacher',
+//      ['Micheal', 'Peter', 'Steven']
+// ];
+
+
+// const types = [];
+
+
+// for(let i = 0; i < jonas.length; i++){
+//     //reaading types array
+//     console.log(jonas[i], typeof jonas[i]);
+
+//     //fillling types array
+//     // types[i] = typeof jonas[i];
+
+//     types.push(typeof jonas[i]);
+    
+// }
+
+// console.log(types)
+
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++){
+//     ages.push(2037 - years[i]);
+// }
+
+// console.log(ages)
+
+
+
+// // Continue and break
+// console.log("=======ONLY STRINGS ===========")
+// for(let i = 0; i < jonas.length; i++){
+//     if(typeof jonas[i] !== 'string') continue;
+//     console.log(jonas[i], typeof jonas[i]);
+// }
+
+// // Continue and break
+// console.log("=======break with number===========")
+// for(let i = 0; i < jonas.length; i++){
+//     if(typeof jonas[i] === 'number') continue;
+//     console.log(jonas[i], typeof jonas[i]);
+// }
+
+
+
+/////LOOPING BACKWARDS AND LOOPS IN LOOPS ////////////
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//      2037 - 1991,
+//      'teacher',
+//      ['Micheal', 'Peter', 'Steven']
+// ];
+
+
+// for(let i = jonas.length - 1; i >= 0; i--){
+//     console.log(i, jonas[i]);
+// }
+
+// for(let exercise = 1; exercise < 4; exercise++){
+//     console.log(`----------Starting exercise ${exercise} `)
+
+//     for( let rep = 1; rep < 6; rep++){
+//         console.log(`Exercise ${exercise} Lifting weight repetion ${rep} `);
+//     }
+// }
+
+
+//////// WHILE LOOP ////////
+// for(let rep = 1; rep <= 10; rep++){
+//     console.log(`Lifting weights repetiton  ${rep}`);
+// }
+ 
+let rep = 1
+while (rep <=10){
+    console.log(`Lifting weights repetiton  ${rep}`);
+    rep++;
+}
+
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+
+while (dice !== 6){
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice=== 6) console.log('Loop is about to end')
+}

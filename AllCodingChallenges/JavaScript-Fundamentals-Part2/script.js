488194d737067e18b1ce0f0ea87f89f99ea29115
florentsahiti <66284617+florentsahiti@users.noreply.github.com>
2024-03-11 03:24:54 +0100

    loops

--- AllCodingChallenges/JavaScript-Fundamentals-Part2/script.js
+++ AllCodingChallenges/JavaScript-Fundamentals-Part2/script.js
@@ -45,34 +45,68 @@
 
 
 /////// CHALLENGE 3 /////////
-const Mark = {
-    firstname: "Mark",
-    lastname: "Miller",
-    mass : 78,
-    height: 1.69,
+// const Mark = {
+//     firstname: "Mark",
+//     lastname: "Miller",
+//     mass : 78,
+//     height: 1.69,
 
-    calcBMI: function(){
-        this.llogaritBMI = this.mass / (this.height ** 2);
-        return `${this.firstname} has a bmi of ${this.llogaritBMI}`;
-    }
+//     calcBMI: function(){
+//         this.llogaritBMI = this.mass / (this.height ** 2);
+//         return `${this.firstname} has a bmi of ${this.llogaritBMI}`;
+//     }
+// }
+
+// const John = {
+//     firstname: "John",
+//     lastname: "Smith",
+//     mass : 92,
+//     height: 1.95,
+
+//     calcBMI: function(){
+//         this.llogaritBMI = this.mass / (this.height ** 2);
+//         return `${this.firstname} has a bmi of ${this.llogaritBMI}`;
+
+//     }
+// }
+
+// Mark.calcBMI();
+// John.calcBMI();
+// console.log(Mark.llogaritBMI, John.llogaritBMI)
+
+
+// console.log(`${John.llogaritBMI > Mark.llogaritBMI ? "John's Smiths bmi is higher than Marks" : " Mark' Miller's Bmi is Higher than john"}`)
+
+const calcTip = function (bill){
+    return bill >= 50 && bill <= 300? bill * 0.15:
+    bill * 0.2;
 }
 
-const John = {
-    firstname: "John",
-    lastname: "Smith",
-    mass : 92,
-    height: 1.95,
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86,52];
+const tips = [];
+const totals = [];
 
-    calcBMI: function(){
-        this.llogaritBMI = this.mass / (this.height ** 2);
-        return `${this.firstname} has a bmi of ${this.llogaritBMI}`;
-
-    }
+for (let i = 0; i < bills.length; i++){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
 }
 
-Mark.calcBMI();
-John.calcBMI();
-console.log(Mark.llogaritBMI, John.llogaritBMI)
+console.log(bills, tips, totals);
 
 
-console.log(`${John.llogaritBMI > Mark.llogaritBMI ? "John's Smiths bmi is higher than Marks" : " Mark' Miller's Bmi is Higher than john"}`)
+const calcAverage = function(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        // sum = sum + arr[i]
+        sum += arr[i];
+
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage([2,3,6]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+
+

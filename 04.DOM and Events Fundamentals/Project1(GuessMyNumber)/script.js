7bdab50eabde79ebd9dc3c576b7b6071598404bc
florentsahiti <66284617+florentsahiti@users.noreply.github.com>
2024-12-01 23:12:33 +0100

    GuessMyNumber

--- 04.DOM and Events Fundamentals/Project1(GuessMyNumber)/script.js
+++ 04.DOM and Events Fundamentals/Project1(GuessMyNumber)/script.js
@@ -11,14 +11,48 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector)('.guess').value
 
+
+const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if(!guess){
         document.querySelector('.message').textContent = 'No Number'
+    }else if( guess == secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number'
+    }else if( guess > secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Number you guessed is Too High try lower'
+            score = score - 1;
+            document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
 
+        }
+        
+    }else if( guess < secretNumber){
+
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Number you guessed is Too High try lower'
+            score = score - 1;
+            document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+
+        
+        
+    }
+       
     }
 
+    //16:54
+
     // document.querySelector('.message').textContent = 'Correct Number'
-} )
+})
